9a0844ec6fd3252d71daccdcd6589a507b7fa609
mounika <dinesh.cd15@gmail.com>
2026-05-03 12:17:41 +0200

    updated install_nginx_old.sh

--- install_nginx_old.sh
+++ install_nginx_old.sh
@@ -7,4 +7,4 @@ sudo yum install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
-echo "<h1>Hello from Multi-Cloud Image</h1>" | sudo tee /usr/share/nginx/html/index.html
+echo "<h1>Hello! from Multi-Cloud Image</h1>" | sudo tee /usr/share/nginx/html/index.html
