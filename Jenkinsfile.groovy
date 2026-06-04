990c1c3528f4dd9cdff4c932079431fdaa36f6a4
mounika <dinesh.cd15@gmail.com>
2026-06-04 19:56:26 +0200

    Installation & configure redis

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -326,10 +326,14 @@ sudo apt-get install -y gnupg curl
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor --yes
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mongodb-org
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mongodb-org redis-server
 
 sudo systemctl start mongod
 sudo systemctl enable mongod
+
+echo "Starting Redis server..."
+sudo systemctl start redis-server
+sudo systemctl enable redis-server
 
 echo "Waiting for MongoDB to start..."
 sleep 10
